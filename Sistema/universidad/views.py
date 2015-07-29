f36c09d4784a60b7d8e2b5977118f9fdcc189732
omegasoft <leopoldodibiro@gmail.com>
2015-07-29 10:09:46 -0430

    respuesta de las vistas

--- Sistema/universidad/views.py
+++ Sistema/universidad/views.py
@@ -105,18 +105,19 @@ def estudiante(request):
 
     return HttpResponse(json_data, content_type='application/json')
 
-def estudiante(request):
 
-    if request.POST['nombre']:
+def materias(request):
+
+    if request.POST['nombre_materia']:
 
         diccionario_consulta = {
-            'nombre': request.POST['nombre'],
+            'nombre': request.POST['nombre_materia'],
         }
         lista_consulta.append(diccionario_consulta)
         diccionario_consulta = {}
     else:
         diccionario_consulta = {
-            'nombre': 'Falta Colocar el Nombre',
+            'nombre': 'Falta Colocar el Nombre de la Materia',
         }
         lista_consulta.append(diccionario_consulta)
         diccionario_consulta = {}
@@ -125,3 +126,36 @@ def estudiante(request):
 
     return HttpResponse(json_data, content_type='application/json')
 
+
+def asignacion(request):
+
+    if request.POST['id_materia']:
+
+        diccionario_consulta = {
+            'id_materia': request.POST['id_materia'],
+        }
+        lista_consulta.append(diccionario_consulta)
+        diccionario_consulta = {}
+        if request.POST['id_estudiante']:
+
+            diccionario_consulta = {
+                'id_estudiante': request.POST['id_estudiante'],
+            }
+            lista_consulta.append(diccionario_consulta)
+            diccionario_consulta = {}
+        else:
+            diccionario_consulta = {
+                'id_estudiante': 'Falta Colocar el estudiante',
+            }
+            lista_consulta.append(diccionario_consulta)
+            diccionario_consulta = {}
+    else:
+        diccionario_consulta = {
+            'id_materia': 'Falta Colocar la Materia',
+        }
+        lista_consulta.append(diccionario_consulta)
+        diccionario_consulta = {}
+
+    json_data = json.dumps(lista_consulta)
+
+    return HttpResponse(json_data, content_type='application/json')
