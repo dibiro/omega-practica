0adf1de0bd2c42865fe97a5c6f9cf27ddf4c5c58
erifranckN <erifranckjose@gmail.com>
2015-07-29 14:02:05 -0430

    se esta capturando el json

--- Sistema/universidad/static/js/main.js
+++ Sistema/universidad/static/js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	$('#estudiantes').dataTable();
-	$('#materia').dataTable();
-	$('#asignatura').dataTable();
+	var tablaEstudiante= $('#estudiantes').dataTable();
+	var tablaMateria= $('#materia').dataTable();
+	var tablaAsignatura= $('#asignatura').dataTable();
 
 	$.getJSON('/unerg/estudiante_json', function(json, textStatus) {
 		$.each(json, function(index, val) {
 			var row = $('<tr />')
-			 $("<td />").text(val.first_name).appendTo(row)
+			/** $("<td />").text(val.first_name).appendTo(row)
 			 $("<td />").text(val.last_name).appendTo(row)
 			 $("<td />").text(val.cedula).appendTo(row)
 			 $("<td />").text(val.edad).appendTo(row)
 			 $("<td />").text(val.email).appendTo(row)
-			 row.appendTo('#estudiantes');
+			 row.appendTo('#estudiantes');**/
 		});
 	});
 });
