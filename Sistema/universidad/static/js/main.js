09132056b9cfc1bb84c5c63b5cd406b3fc4cdd1a
erifranckN <erifranckjose@gmail.com>
2015-07-29 13:53:26 -0430

    se esta insertando el json en la datatable

--- Sistema/universidad/static/js/main.js
+++ Sistema/universidad/static/js/main.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
 	$('#estudiantes').dataTable();
 	$('#materia').dataTable();
 	$('#asignatura').dataTable();
+
 	$.getJSON('/unerg/estudiante_json', function(json, textStatus) {
 		$.each(json, function(index, val) {
-			 console.log(val.first_name)
-			 console.log(val.last_name)
-			 console.log(val.cedula)
-			 console.log(val.edad)
-			 console.log(val.email)
+			var row = $('<tr />')
+			 $("<td />").text(val.first_name).appendTo(row)
+			 $("<td />").text(val.last_name).appendTo(row)
+			 $("<td />").text(val.cedula).appendTo(row)
+			 $("<td />").text(val.edad).appendTo(row)
+			 $("<td />").text(val.email).appendTo(row)
+			 row.appendTo('#estudiantes');
 		});
 	});
 });
