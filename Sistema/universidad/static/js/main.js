b6d53651beaa7bfbd0ee409828d5c969efb09b10
omegasoft <leopoldodibiro@gmail.com>
2015-07-29 13:37:15 -0430

    envio de json estudiante

--- Sistema/universidad/static/js/main.js
+++ Sistema/universidad/static/js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 	$('#estudiantes').dataTable();
 	$('#materia').dataTable();
 	$('#asignatura').dataTable();
-	$.getJSON('/universidad/estudiante_json', function(json, textStatus) {
+	$.getJSON('/unerg/estudiante_json', function(json, textStatus) {
 		$.each(json, function(index, val) {
 			 console.log(val.first_name)
 			 console.log(val.last_name)
+			 console.log(val.cedula)
 			 console.log(val.edad)
 			 console.log(val.email)
 		});
