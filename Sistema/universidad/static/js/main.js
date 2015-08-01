9e8b9ae641aceee7b2275abe2a5c363ba89af5ca
erifranckjose@gmail.com <erifranckjose@gmail.com>
2015-07-31 20:42:30 -0430

    añadiendo scroll

--- Sistema/universidad/static/js/main.js
+++ Sistema/universidad/static/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(document).ready(function() {
 
     asignatura.hide();
     fill_student();
@@ -6,13 +6,13 @@ $(document).ready(function() {
     evento_click_guardarUsuario();
 
 })
+
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie != '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -24,43 +24,43 @@ function getCookie(name) {
 var csrftoken = getCookie('csrftoken');
 
 
-var asignatura= $('.contenedor-asignatura');
-var tablaEstudiante= $('#estudiantes').dataTable({
-    'scrollX':true
+var asignatura = $('.contenedor-asignatura');
+var tablaEstudiante = $('#estudiantes').dataTable({
+    'scrollX': true
 });
 
-var tablaMateria= $('#materia').dataTable({
-    'sScrollInfinite':true,
+var tablaMateria = $('#materia').dataTable({
+    "scrollY": "200px",
+    "scrollCollapse": true,
 });
 
-var tablaAsignatura= $('#asignaturas_asociadas').dataTable({
-    'scrollX':true,
-    'bPaginate':false,
-    'searching':false,
+var tablaAsignatura = $('#asignaturas_asociadas').dataTable({
+    'scrollX': true,
+    'bPaginate': false,
+    'searching': false,
 });
-var tablaAsignaturaNoAsociadas= $('#asignaturas_noAsociadas').dataTable({
-    'sScrollInfinite':true,
-    'bPaginate':false,
-    'searching':false,
+var tablaAsignaturaNoAsociadas = $('#asignaturas_noAsociadas').dataTable({
+    'sScrollInfinite': true,
+    'bPaginate': false,
+    'searching': false,
 });
 
-function fill_student () {
+function fill_student() {
     tablaEstudiante.fnClearTable();
     $.getJSON('/unerg/estudiante_json', function(json, textStatus) {
-        $.each(json, function(index, val) {		
-            var addData=[];
+        $.each(json, function(index, val) {
+            var addData = [];
             addData.push(val.nombre);
             addData.push(val.apellido);
             addData.push(val.cedula);
             addData.push(val.edad);
             addData.push(val.email);
             if (val.estado) {
-                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id="+val.id_estudiantee+" checked></div>");
+                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id=" + val.id_estudiantee + " checked></div>");
+            } else {
+                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id=" + val.id_estudiantee + "> </div>");
             }
-            else{
-                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id="+val.id_estudiantee+"> </div>");
-            }
-            addData.push("<button class='btn btn-success asignaturas' data-id="+val.id_estudiantee+">asignatura</button>");
+            addData.push("<button class='btn btn-success asignaturas' data-id=" + val.id_estudiantee + ">asignatura</button>");
             tablaEstudiante.fnAddData(addData);
 
         });
@@ -72,19 +72,18 @@ function fill_student () {
     })
 }
 
-function fill_materia(){
+function fill_materia() {
     tablaMateria.fnClearTable()
     $.getJSON('/unerg/materias_json', function(json, textStatus) {
         $.each(json, function(index, val) {
-            var addData=[];
+            var addData = [];
             addData.push(val.nombre);
             if (val.estado) {
-                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id="+val.id_materia+" checked></div>");
+                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id=" + val.id_materia + " checked></div>");
+            } else {
+                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id=" + val.id_materia + "> </div>");
             }
-            else{
-                addData.push("<div class='make-switch estado' ><input type='checkbox' class='status' data-id="+val.id_materia+"> </div>");
-            }
-            tablaMateria.fnAddData(addData);	
+            tablaMateria.fnAddData(addData);
         });
         instanciandoSwitch();
         evento_click_asociaciones('/unerg/eliminar_materia_json/');
@@ -93,14 +92,14 @@ function fill_materia(){
 
 }
 
-function fill_asignaturas(id){
+function fill_asignaturas(id) {
     tablaAsignatura.fnClearTable();
     tablaAsignaturaNoAsociadas.fnClearTable();
-    $.getJSON('/unerg/materias_asociadas_estudiante_json/'+id, function(json, textStatus) {
+    $.getJSON('/unerg/materias_asociadas_estudiante_json/' + id, function(json, textStatus) {
         $.each(json, function(index, val) {
-            var addData=[];
+            var addData = [];
             addData.push(val.materia_asignada);
-            addData.push("<div class='make-switch id_asignacion' data-id='"+id+"'><input type='checkbox'  class='desasigna' data-id='"+val.id_asignacion  +"' checked></div>")
+            addData.push("<div class='make-switch id_asignacion' data-id='" + id + "'><input type='checkbox'  class='desasigna' data-id='" + val.id_asignacion + "' checked></div>")
             tablaAsignatura.fnAddData(addData);
         });
         instanciandoSwitch();
@@ -109,11 +108,11 @@ function fill_asignaturas(id){
 
 
     });
-    $.getJSON('/unerg/materias_no_asociadas_json/'+id, function(json, textStatus) {
+    $.getJSON('/unerg/materias_no_asociadas_json/' + id, function(json, textStatus) {
         $.each(json, function(index, val) {
-            var addData=[];
+            var addData = [];
             addData.push(val.materia_no_asignada);
-            addData.push("<div class='make-switch id_asignacion'data-id='"+id+"'><input type='checkbox'  class='asigna' data-id='"+val.codigo_materia+"'></div>")
+            addData.push("<div class='make-switch id_asignacion'data-id='" + id + "'><input type='checkbox'  class='asigna' data-id='" + val.codigo_materia + "'></div>")
             tablaAsignaturaNoAsociadas.fnAddData(addData);
         });
         instanciandoSwitch();
@@ -122,112 +121,120 @@ function fill_asignaturas(id){
 
     });
 }
-function update_state(id,direccion){
-    $.get(direccion+id);
+
+function update_state(id, direccion) {
+    $.get(direccion + id);
 
 }
-function update_relation(id_estudiante , id_materia) {
+
+function update_relation(id_estudiante, id_materia) {
 
     $.ajax({
-        url: '/unerg/asignacion_guardar_json/'+id_estudiante,
-        type: 'POST',
-        dataType: 'json',
-        data: {'codigo_materia':id_materia },
-        beforeSend: cook()
-    })
-    .done(function() {
-        console.log("success");
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        fill_asignaturas(id_estudiante);
-    });
-    function cook(xhr) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        
-        };
-
-}
-function update_desasigna(id_materia,id_estudiante) {
-
-    $.get('/unerg/asignacion_eliminar_json/'+id_materia, function(){
-        fill_asignaturas(id_estudiante);
-    });
-
-}
-function Agregando(direccion,valores){
-    $.ajax({
-        url: direccion,
-        type: 'POST',
-        dataType: 'json',
-        data: valores,
+            url: '/unerg/asignacion_guardar_json/' + id_estudiante,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'codigo_materia': id_materia
+            },
+            beforeSend: cook()
         })
-    .done(function() {
+        .done(function() {
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            fill_asignaturas(id_estudiante);
+        });
 
+    function cook(xhr) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
 
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        fill_materia();
-        fill_student();
+    };
+
+}
+
+function update_desasigna(id_materia, id_estudiante) {
+
+    $.get('/unerg/asignacion_eliminar_json/' + id_materia, function() {
+        fill_asignaturas(id_estudiante);
     });
+
+}
+
+function Agregando(direccion, valores) {
+    $.ajax({
+            url: direccion,
+            type: 'POST',
+            dataType: 'json',
+            data: valores,
+        })
+        .done(function() {
+
+
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            fill_materia();
+            fill_student();
+        });
 
 
 };
 
-function evento_click_estudiante () {
+function evento_click_estudiante() {
     $('.asignaturas').unbind('click');
     $('.asignaturas').on('click', function() {
         asignatura.show(2000);
         fill_asignaturas($(this).data('id'));
     });
 }
-function instanciandoSwitch(){
+
+function instanciandoSwitch() {
     $(".status").bootstrapSwitch({
-        'size':'mini',
-        'onColor':'success',
-        'offColor':'default',
-        'handleWidth':10,
-        'labelWidth':5,
-        'onText':"<i class='icon-user-check'></i>",
-        'offText':"<i class='icon-cross'></i>"
+        'size': 'mini',
+        'onColor': 'success',
+        'offColor': 'default',
+        'handleWidth': 10,
+        'labelWidth': 5,
+        'onText': "<i class='icon-user-check'></i>",
+        'offText': "<i class='icon-cross'></i>"
     });
     $(".asigna,.desasigna").bootstrapSwitch({
-        'size':'mini',
-        'onColor':'success',
-        'offColor':'default',
-        'handleWidth':10,
-        'labelWidth':5,
-        'onText':"<i class='icon-check'></i>",
-        'offText':"<i class='icon-cross'></i>"
+        'size': 'mini',
+        'onColor': 'success',
+        'offColor': 'default',
+        'handleWidth': 10,
+        'labelWidth': 5,
+        'onText': "<i class='icon-check'></i>",
+        'offText': "<i class='icon-cross'></i>"
     });
 
 }
 
-function evento_click_asociaciones(url){
+function evento_click_asociaciones(url) {
 
     $('.status').on('switchChange.bootstrapSwitch', function(event) {
-        update_state($(this).data('id'),url);
+        update_state($(this).data('id'), url);
     });
-  
+
 }
 
-function evento_click_asigna(){
-    $('.asigna').on('switchChange.bootstrapSwitch',function(event){
-        update_relation($('.id_asignacion').data('id'),$(this).data('id'));
+function evento_click_asigna() {
+    $('.asigna').on('switchChange.bootstrapSwitch', function(event) {
+        update_relation($('.id_asignacion').data('id'), $(this).data('id'));
 
     });
-    $('.desasigna').on('switchChange.bootstrapSwitch',function(event){
-        update_desasigna($(this).data('id'),$('.id_asignacion').data('id'));
+    $('.desasigna').on('switchChange.bootstrapSwitch', function(event) {
+        update_desasigna($(this).data('id'), $('.id_asignacion').data('id'));
 
     })
 }
 
-function evento_click_guardarUsuario(){
+function evento_click_guardarUsuario() {
     $('.agregar').submit(function(event) {
         Agregando($(this).attr('action'), $(this).serialize());
         return false;
